c5c6e7a8a16713e8d8eced8da7fb350d3896e6e1
Martin Pham <martinp0512@gmail.com>
2022-01-12 23:08:01 +0000

    add on click events to buttons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,32 @@
 // Client facing scripts here
+
+$(document).ready(function() {
+
+  const order = [];
+  $('.submit-btn').on('click', function(event) {
+    event.preventDefault();
+    const newObj = {};
+    const id = $(this).attr('id');
+    newObj.qty = $(this).parent().siblings('.price-qty').children('form').children('input').val();
+    newObj.id = id;
+    order.push(newObj);
+    // const randNum = Math.floor(Math.random() * (10000 - 1) + 1);
+    // order[randNum] = newObj;
+  })
+  $('.checkout-btn').on('click', function() {
+    console.log('data.stringify', JSON.stringify(order));
+    $.ajax({
+      url: '/checkout',
+      method: "POST",
+      data: { data: JSON.stringify(order) },
+      success: function() {
+        console.log('success');
+      }
+    });
+
+
+
+  })
+
+});
+
